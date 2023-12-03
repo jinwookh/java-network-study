a84d0bdc27749024785dda664112c15eeb548c41
jinwook han <jin942002@naver.com>
2023-12-03 21:47:59 +0900

    add streaming example code

---
+++ byteStream/LearnJavaIoNioApplication2.java
@@ -0,0 +1,34 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class LearnJavaIoNioApplication2 {
+    public static void main(String[] args) throws Exception {
+        InputStream in = new FileInputStream("test.dat"); // filePath
+        OutputStream out = new FileOutputStream("output.dat"); // filePath
+        BufferedInputStream bin = new BufferedInputStream(in);
+        BufferedOutputStream bout = new BufferedOutputStream(out);
+
+        int copyByte = 0;
+        int bData;
+        long stime = System.currentTimeMillis();
+        while(true) {
+            bData = bin.read();
+            if(bData == -1)
+                break;
+            bout.write(bData);
+            copyByte++;
+        }
+        long etime = System.currentTimeMillis();
+        bin.close();
+        bout.close();
+        System.out.println("---------------버퍼 스트림 전송시간--------------------");
+        System.out.println("복사 시간: "+(etime-stime));
+        System.out.println("복사된 바이트의 크기: "+copyByte);
+
+
+    }
+}
---
+++ byteStream/LearnJavaIoNioApplicationWithSingleByte.java
@@ -0,0 +1,29 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class LearnJavaIoNioApplicationWithSingleByte {
+    public static void main(String[] args) throws Exception {
+        InputStream in = new FileInputStream("test.dat"); // filePath
+        OutputStream out = new FileOutputStream("output.dat"); // filePath
+        int copyByte = 0;
+        int data;
+        long stime = System.currentTimeMillis();
+        byte[] buffer = new byte[512];
+        while(true) {
+            data = in.read();
+            if(data==-1)
+                break;
+            out.write(data);
+            copyByte++;
+        }
+        long etime = System.currentTimeMillis();
+        in.close();
+        out.close();
+        System.out.println("복사 시간: "+(etime-stime));
+        System.out.println("복사된 배열의 크기: "+copyByte);
+
+
+    }
+}
